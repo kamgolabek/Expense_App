5d9c56a1b4d1670390d6b2771cc79db18a5ec15a
Kamil Gołąbek <kamgolabek@gmail.com>
2020-01-28 20:40:13 +0100

    chart widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import './widgets/chart.dart';
+
 import './widgets/new_transaction.dart';
 
 import 'package:flutter/material.dart';
@@ -64,9 +66,19 @@ class _MyHomePageState extends State<MyHomePage> {
     //   id: '2',
     //   title: 'New bike',
     //   amount: 339.99,
-    //   date: DateTime.now(), 
+    //   date: DateTime.now(),
     // ),
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        )
+      );
+    }).toList();
+  }
 
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
@@ -88,7 +100,6 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(
           'Personal Expenses',
           style: TextStyle(fontFamily: 'OpenSans'),
-          
         ),
         actions: <Widget>[
           IconButton(
@@ -102,14 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART'),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
